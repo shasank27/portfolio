5f02b59eae60555adba6b4b85fab492eb2c3dac3
shasank27 <periwalshasank2027@gmail.com>
2022-12-23 19:21:38 +0530

    More updates

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./nav.css";
+import { AiOutlineHome, AiOutlineUser, AiOutlineMessage } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#" className="active">
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <BiBook />
+      </a>
+      <a href="#project">
+        <RiServiceLine />
+      </a>
+      <a href="#contact">
+        <AiOutlineMessage />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
---
+++ src/components/projects/Project.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Project = () => {
+  return <section id="project">Project</section>;
+};
+
+export default Project;
