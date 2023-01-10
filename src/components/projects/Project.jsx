062f36945f0eac9b005f44224205db8953deee5c
shasank27 <periwalshasank2027@gmail.com>
2023-01-10 23:22:40 +0530

    Project section started

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Project = () => {
-  return <section id="project">Project</section>;
+  return (
+    <section id="project">
+      <h5>What projects I made</h5>
+      <h2>My Projects</h2>
+    </section>
+  );
 };
 
 export default Project;
